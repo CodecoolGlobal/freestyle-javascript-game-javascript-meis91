208e51e1d087b0cbfe0131e5bcd5b7a6f1bd9eaf
Katharina Vogler <kvog82@gmail.com>
2022-08-17 16:46:07 +0200

    welcome page

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import data_manager
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-
 @app.route("/", methods=['GET', 'POST'])
-def index():
+def welcome():
+    if request.method == 'POST':
+        user_name = request.form["user_name"]
+        return index(user_name)
+    return render_template('welcome.html')
+
+@app.route("/index", methods=['GET', 'POST'])
+def index(user_name=None):
     score_list = data_manager.get_highscore_list()
-    return render_template('index.html', score_list=score_list)
+    return render_template('index.html', score_list=score_list, user_name=user_name)
+
 
 
 if __name__ == "__main__":
